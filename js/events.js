7b98dd94382cec3b33e7d06699b55c9926cc7223
ilemarandrade <ilemarandrade@gmail.com>
2024-01-10 10:30:28 -0400

    feat: add animations of header and nav

--- js/events.js
+++ js/events.js
@@ -5,10 +5,13 @@ $(document).ready(function(){
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
     if(scroll > 50){
-      $('header').css('background', 'blue');
+      $("#header").removeClass("bg-primary").addClass("bg-light")
+      $('#header').find('.text-white').removeClass('text-white').addClass('text-primary');
+      $("#navbar").addClass("bg-primary");
     } else{
-      $('header').css('background', 'transparent');
-      
+      $("#header").addClass("bg-primary").removeClass("bg-light");
+      $('#header').find('.text-primary').removeClass('text-primary').addClass('text-white');;
+      $("#navbar").removeClass("bg-primary");
     }
   });
 });
