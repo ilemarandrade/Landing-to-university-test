7f0999494230c2e1434a9a022fcc52d6a2e08c36
ilemarandrade <ilemarandrade@gmail.com>
2024-01-11 10:35:02 -0400

    fix scroll when was clicked a anchor

--- js/events.js
+++ js/events.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
+$(document).ready(function () {
+  $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     const isCollapsedMenu = $(".navbar-toggler").attr('aria-expanded') === "false";
 
-    if(!isCollapsedMenu){
+    if (!isCollapsedMenu) {
       return
     }
 
-    if(scroll > 50){
+    if (scroll > 50) {
       $("#header").removeClass("bg-primary").addClass("bg-white")
       $('#header').find('.text-white').removeClass('text-white').addClass('text-primary');
       $("#navbar").addClass("bg-primary");
-    } else{
+    } else {
       $("#header").addClass("bg-primary").removeClass("bg-white");
       $('#header').find('.text-primary').removeClass('text-primary').addClass('text-white');;
       $("#navbar").removeClass("bg-primary");
@@ -24,18 +24,30 @@ $(document).ready(function(){
     video.play();
   })
 
-  $(".navbar-toggler").click(function(){
+  $(".navbar-toggler").click(function () {
     const isCollapsedMenu = $(".navbar-toggler").attr('aria-expanded') === "false";
 
-    if(isCollapsedMenu){
+    if (isCollapsedMenu) {
       $("#header").removeClass("bg-primary").addClass("bg-white")
       $('#header').find('.text-white').removeClass('text-white').addClass('text-primary');
       $("#navbar").addClass("bg-primary");
-    } else{
+    } else {
       $("#header").addClass("bg-primary").removeClass("bg-white");
       $('#header').find('.text-primary').removeClass('text-primary').addClass('text-white');;
       $("#navbar").removeClass("bg-primary");
     }
-  })
+  });
+
+  $('a[href^="#"]').click(function (event) {
+    event.preventDefault();
+    var isMobile = $(window).width() < 992;
+    var isCollapsedMenu = $(".navbar-toggler").attr('aria-expanded') === "false";
+    var addPx = isCollapsedMenu || !isMobile ? 100:250;
+    var ancla = this.hash;
+    var desplazamiento = $(ancla).offset().top - addPx; // Cambia '100' a la cantidad de píxeles que quieras desplazar hacia abajo
+ 
+    window.scrollTo({ top: desplazamiento, behavior: 'smooth' });
+  });
+
 
 });
